917549f15088bfef74548f172f81e20f8e88e6f3
BritishPandaChick <sbartley1080@gmail.com>
2020-03-10 16:54:55 -0400

    Change assert fail to assert equal for tests. Add chai-http request code

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -139,11 +139,19 @@ suite('Functional Tests', function() {
       // Try it again. This time without help !!
       test('send {surname: "da Verrazzano"}', function(done) {
         /** place the chai-http request code here... **/
-        
+        chai.request(server)
         /** place your tests inside the callback **/
+          .put('/travellers')
+          .send({surname: 'da Verrrazzano'})
+          .end(function(err, res){
+            assert.equal(res.status, 200, 'response status should be 200');
+            assert.equal(res.type, 'application/json', 'Response should be json');
+            assert.equal(res.body.name, 'Giovanni');
+            assert.equal(res.body.surname, 'da Verrazzano', 'res.body.surname should be "da Verrazzano"');
+            
+            done();
+          });
         
-        assert.fail(); // remove this after adding tests
-        done();
       });
     });
 
